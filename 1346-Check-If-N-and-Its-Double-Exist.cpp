af10769cd78cd4b939b74c79c08717f42465b4e2
Pratham Chhabra <87524634+pchhabra07@users.noreply.github.com>
2026-09-08 19:56:45 +0530

    [1346] [Time Beats: 100.00%] [Memory Beats: 5.79%] - LeetPush

--- 1346-Check-If-N-and-Its-Double-Exist.cpp
+++ 1346-Check-If-N-and-Its-Double-Exist.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
         int n = arr.size();
-        map<int,int>mp;
+        unordered_map<int,int>mp;
 
         for(int i = 0; i < n; i++){
             mp[arr[i]] = i;
