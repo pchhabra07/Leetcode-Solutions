8cc538c3d9fe05260361ce337727ff17d7cfd5d8
Pratham Chhabra <87524634+pchhabra07@users.noreply.github.com>
2026-09-12 16:53:11 +0530

    [1299] [Time Beats: 100.00%] [Memory Beats: 95.43%] - LeetPush

---
+++ 1299-Replace-Elements-with-Greatest-Element-on-Right-Side.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int n = arr.size();
+
+        int maxNum = arr[n-1];
+        arr[n-1] = -1;
+
+        for(int i = n-2; i >=0; i--){
+            int x = arr[i];
+            arr[i] = maxNum;
+            maxNum = max(maxNum,x);
+        }
+
+        return arr;
+    }
+};
